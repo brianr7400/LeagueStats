47ca9a5c8943e7ca657d4c1d5366aab491aa3d28
brianr7400 <brianr7400@gmail.com>
2015-07-15 23:07:39 -0400

    Added the beginning of the project(Still not functional)

---
+++ LeagueStats/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace LeagueStats
+{
+    partial class LeagueStats
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.statusBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.iconBox = new System.Windows.Forms.PictureBox();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(384, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusBar});
+            this.statusStrip.Location = new System.Drawing.Point(0, 339);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(384, 22);
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // statusBar
+            // 
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(100, 16);
+            // 
+            // searchButton
+            // 
+            this.searchButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.searchButton.ForeColor = System.Drawing.Color.Black;
+            this.searchButton.Location = new System.Drawing.Point(297, 30);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "NA"});
+            this.comboBox1.Location = new System.Drawing.Point(7, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(61, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Lucida Sans", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(10, 64);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(292, 39);
+            this.nameLabel.TabIndex = 5;
+            this.nameLabel.Text = "SummonerName";
+            this.nameLabel.UseMnemonic = false;
+            this.nameLabel.Visible = false;
+            // 
+            // iconBox
+            // 
+            this.iconBox.ImageLocation = "";
+            this.iconBox.Location = new System.Drawing.Point(297, 64);
+            this.iconBox.Name = "iconBox";
+            this.iconBox.Size = new System.Drawing.Size(75, 75);
+            this.iconBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.iconBox.TabIndex = 6;
+            this.iconBox.TabStop = false;
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(74, 29);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(217, 20);
+            this.searchBox.TabIndex = 7;
+            this.searchBox.Text = "Summoner Name";
+            this.searchBox.Click += new System.EventHandler(this.searchBox_Click);
+            this.searchBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.searchBox_KeyDown);
+            // 
+            // LeagueStats
+            // 
+            this.AcceptButton = this.searchButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.iconBox);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "LeagueStats";
+            this.Text = "LeagueStats";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripProgressBar statusBar;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        public System.Windows.Forms.PictureBox iconBox;
+        public System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox searchBox;
+    }
+}
+
---
+++ LeagueStats/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+namespace LeagueStats
+{
+    public partial class LeagueStats : Form
+    {
+        #region Global Variables
+
+        static string SummonerName;
+        static User currentUser = new User();
+
+        #endregion
+        public LeagueStats()
+        {
+            InitializeComponent();
+
+        }
+        #region MenuStrip
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string AboutMessage = String.Format("LeagueStats\r\nCreatd By Brian Richardson");
+            MessageBox.Show(AboutMessage);
+        }
+        #endregion
+        #region methods
+        public static void CallAPI()
+        {
+            
+            //Creates WebClient
+            var Client = new WebClient();
+            //Gets Summoner information | name, id, profileIconId, summonerLevel, revisionDate
+            try
+            {
+                string url = ("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + SummonerName.Replace(" ", "%20") + "?api_key=c19aabb4-0d8e-44c1-ae83-4b03249382e9");
+                var SummonerData = Client.DownloadString(url);
+                //Puts summoner information into json
+                JObject jsonSummonerData = JObject.Parse(SummonerData);
+                //Removes spaces from variable SummonerName 
+                SummonerName = SummonerName.Replace(" ", "");
+                //Sets currentusers properties to the correct json ones;
+                currentUser.name = (string)(jsonSummonerData[SummonerName]["name"]);
+                currentUser.id = (string)(jsonSummonerData[SummonerName]["id"]);
+                currentUser.profileIconId = (string)(jsonSummonerData[SummonerName]["profileIconId"]);
+                currentUser.summonerLevel = (string)(jsonSummonerData[SummonerName]["summonerLevel"]);
+                currentUser.revisionDate = (string)(jsonSummonerData[SummonerName]["revisionDate"]);
+            }
+            catch
+            {
+                MessageBox.Show("There was an error. Perhaps try another username");
+            }
+            
+        }
+        public static void Display(PictureBox iconBox, Label nameLabel)
+        {
+            //Changes name label & makes it visible
+            nameLabel.Visible = true;
+            nameLabel.Text = currentUser.name;
+            //changes iconBox image location
+            iconBox.ImageLocation = "http://ddragon.leagueoflegends.com/cdn/5.2.1/img/profileicon/" + currentUser.profileIconId + ".png";
+        }
+        #endregion
+
+        #region Form Controlls
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            //Gets summoner name
+            SummonerName = searchBox.Text.ToLower();
+            //Runs the CallAPI method
+            CallAPI();
+            //Displays the data using the Display method
+            Display(iconBox,nameLabel);
+
+        }
+        //Accepts enter key for searching
+        private void searchBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                searchButton_Click(null, null);
+            }
+        }
+        private void searchBox_Click(object sender, EventArgs e)
+        {
+            //Clears searchBox when clicked
+            searchBox.Clear();
+        }
+        #endregion
+    }
+    public class User
+    {
+        public string id;
+        public string name;
+        public string profileIconId;
+        public string revisionDate;
+        public string summonerLevel;
+    }
+}
